140e392cb3dd6f7794c123e36b182c0bb7552916
Carlos Lagrange <cildelfino@gmail.com>
2013-06-12 10:10:42 +0100

    programv1

---
+++ Firstprogram.rb
@@ -0,0 +1,7 @@
+puts "hello World"
+puts "hello Again" 
+puts "I like typing like this"
+puts "This is fun."
+puts "Yay! for Printing"
+#puts "I'd rather you 'not'"
+#puts 'I "said" do not touch this'
