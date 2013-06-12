70f9eee7e7265b6366e0560b9be7ab7f3fa6618e
Carlos Lagrange <cildelfino@gmail.com>
2013-06-12 10:50:11 +0100

    programv3

--- Firstprogram.rb
+++ Firstprogram.rb
@@ -48,5 +48,30 @@ puts "We have transport #{passengers} passengers to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
 #End of exercise 4
 
+#Start of exercise 5
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_kweight= my_weight * 0.45 
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about %s." % my_name
+puts "He's %d inches tall." % my_height
+puts "He's %d pounds heavy." % my_weight
+puts "That is #{my_kweight} in kilos"
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+
+puts "If I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age + my_height + my_weight]
+#Why just not put the variables inside the strings?
+#End of exercise 5
+
+#Start of exercise 6
+
+
 
 
