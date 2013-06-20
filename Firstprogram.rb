6d118c54ec53bbc90b157fe5dc1c1bb4cc2eb3aa
Carlos Lagrange <cildelfino@gmail.com>
2013-06-20 15:37:14 +0100

    programv19

--- Firstprogram.rb
+++ Firstprogram.rb
@@ -800,94 +800,180 @@ end
 
 #Doodle -----------------------------------------------------------------
 
-def prompt()
-  print "> "
-end
+# def prompt()
+#   print "> "
+# end
 
-def gold_room()
-  puts "This room is full of gold.  How much do you take?"
+# def gold_room()
+#   puts "This room is full of gold.  How much do you take?"
 
-  prompt; next_move = gets.chomp
-  if next_move.include? "0" or next_move.include? "1"
-    how_much = next_move.to_i()
-  else
-    dead("Man, learn to type a number.")
+#   prompt; next_move = gets.chomp
+#   if next_move.include? "0" or next_move.include? "1"
+#     how_much = next_move.to_i()
+#   else
+#     dead("Man, learn to type a number.")
+#   end
+
+#   if how_much < 50
+#     puts "Nice, you're not greedy, you win!"
+#     Process.exit(0)
+#   else
+#     dead("You greedy bastard!")
+#   end
+# end
+
+#Start of exercise 35 -----------------------------------------------------------------
+
+# def prompt()
+#   print "> "
+# end
+
+# def gold_room()
+#   puts "This room is full of gold.  How much do you take?"
+
+#   prompt; next_move = gets.chomp
+#   if next_move.include? "0" or next_move.include? "1"
+#     how_much = next_move.to_i()
+#   else
+#     dead("Man, learn to type a number.")
+#   end
+
+#   if how_much < 50
+#     puts "Nice, you're not greedy, you win!"
+#     Process.exit(0)
+#   else
+#     dead("You greedy bastard!")
+#   end
+# end
+
+
+# def bear_room()
+#   puts "There is a bear here."
+#   puts "The bear has a bunch of honey."
+#   puts "The fat bear is in front of another door."
+#   puts "How are you going to move the bear?"
+#   bear_moved = false
+
+#   while true
+#     prompt; next_move = gets.chomp
+
+#     if next_move == "take honey"
+#       dead("The bear looks at you then slaps your face off.")
+#     elsif next_move == "taunt bear" and not bear_moved
+#       puts "The bear has moved from the door. You can go through it now."
+#       bear_moved = true
+#     elsif next_move == "taunt bear" and bear_moved
+#       dead("The bear gets pissed off and chews your leg off.")
+#     elsif next_move == "open door" and bear_moved
+#       gold_room()
+#     else
+#       puts "I got no idea what that means."
+#     end
+#   end
+# end
+
+# def cthulhu_room()
+#   puts "Here you see the great evil Cthulhu."
+#   puts "He, it, whatever stares at you and you go insane."
+#   puts "Do you flee for your life or eat your head?"
+
+#   prompt; next_move = gets.chomp
+
+#   if next_move.include? "flee"
+#     start()
+#   elsif next_move.include? "head"
+#     dead("Well that was tasty!")
+#   else
+#     cthulhu_room()
+#   end
+# end
+
+# def dead(why)
+#   puts "#{why}  Good job!"
+#   Process.exit(0)
+# end
+
+# def start()
+#   puts "You are in a dark room."
+#   puts "There is a door to your right and left."
+#   puts "Which one do you take?"
+
+#   prompt; next_move = gets.chomp
+
+#   if next_move == "left"
+#     bear_room()
+#   elsif next_move == "right"
+#     cthulhu_room()
+#   else
+#     dead("You stumble around the room until you starve.")
+#   end
+# end
+
+
+#End of exercise 35 -----------------------------------------------------------------
+
+#Start of exercise 36 -----------------------------------------------------------------
+
+def pokemonbattle
+
+	puts "A wild lvl. 31 Pikachu has appeared! Entering battle!"
+	puts "Choose your pokemon:"
+	puts "1.Squirtle"
+	puts "2.Charmander"
+
+
+	choice = gets.chomp
+
+  if choice == "1"
+    puts "Squirtle I choose you!"
+    battle_squirtle
+  elsif choice == "2"
+  	puts "Charmander I choose you!"
+  else choice != "1" or "2"
+  	puts "Choose a pokemon, 1 or 2"
+  	return false
+
   end
 
-  if how_much < 50
-    puts "Nice, you're not greedy, you win!"
-    Process.exit(0)
-  else
-    dead("You greedy bastard!")
+end
+
+def battle_squirtle
+	puts "Pikachu has used Thunder! It's super effective, Squirtle takes -40hp HP: !!!!!!!!!........ "
+	puts "What's your move?"
+	puts "1. Fight"
+	puts "2. Use Item"
+	puts "3. Run like a little girl"
+
+choice = gets.chomp
+
+  if choice == "1"
+  	fight_method
+  elsif choice == "2"
+  	use_item
+  else choice == "3"
+  	run_method
+
   end
+
 end
 
+def fight_method
+	puts "You've chosen to fight!"
+	puts "What your order:"
+	puts "1. Water blast"
+	puts "2. Shell defense"
+	puts "3. Tackle"
 
-def bear_room()
-  puts "There is a bear here."
-  puts "The bear has a bunch of honey."
-  puts "The fat bear is in front of another door."
-  puts "How are you going to move the bear?"
-  bear_moved = false
-
-  while true
-    prompt; next_move = gets.chomp
-
-    if next_move == "take honey"
-      dead("The bear looks at you then slaps your face off.")
-    elsif next_move == "taunt bear" and not bear_moved
-      puts "The bear has moved from the door. You can go through it now."
-      bear_moved = true
-    elsif next_move == "taunt bear" and bear_moved
-      dead("The bear gets pissed off and chews your leg off.")
-    elsif next_move == "open door" and bear_moved
-      gold_room()
-    else
-      puts "I got no idea what that means."
-    end
-  end
 end
 
-def cthulhu_room()
-  puts "Here you see the great evil Cthulhu."
-  puts "He, it, whatever stares at you and you go insane."
-  puts "Do you flee for your life or eat your head?"
+#pokemonbattle
 
-  prompt; next_move = gets.chomp
 
-  if next_move.include? "flee"
-    start()
-  elsif next_move.include? "head"
-    dead("Well that was tasty!")
-  else
-    cthulhu_room()
-  end
+input = ''
+while input != 'bye' puts input
+input = gets.chomp
 end
-
-def dead(why)
-  puts "#{why}  Good job!"
-  Process.exit(0)
-end
-
-def start()
-  puts "You are in a dark room."
-  puts "There is a door to your right and left."
-  puts "Which one do you take?"
-
-  prompt; next_move = gets.chomp
-
-  if next_move == "left"
-    bear_room()
-  elsif next_move == "right"
-    cthulhu_room()
-  else
-    dead("You stumble around the room until you starve.")
-  end
-end
-
-
-
-
+puts 'Come again soon!'
 
 
 
